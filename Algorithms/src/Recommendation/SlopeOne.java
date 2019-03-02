70d141ef7d8f9b4d009040095323bc7a85bab08c
Sina Sen <sinasen2000@hotmail.com>
2019-03-02 22:53:02 +0100

    added a new class

---
+++ Algorithms/src/Recommendation/SlopeOne.java
@@ -0,0 +1,10 @@
+package Recommendation;
+
+/**
+ * This recommendation algorithm makes use of it is the simplest form of non-trivial item-based collaborative filtering based on ratings.
+ * Its simplicity makes it especially easy to implement it efficiently while its accuracy is often on par with more complicated and computationally expensive algorithms.
+ * Note: The description is taken from Wikipedia
+ */
+public class SlopeOne {
+
+}
