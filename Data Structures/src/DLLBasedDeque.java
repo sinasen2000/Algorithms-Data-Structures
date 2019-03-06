f0a7137bdd465ff5c0202f91ab19d86f2b8a241a
Sina Sen <sinasen2000@hotmail.com>
2019-03-07 00:17:38 +0100

    added DLL Based Queue

--- Data Structures/src/DLLBasedDeque.java
+++ Data Structures/src/DLLBasedDeque.java
@@ -1,2 +1,38 @@
-public class DLLBasedDeque {
+public class DLLBasedDeque<E> {
+    DoublyLinkedList<E> deque = new DoublyLinkedList<>();
+
+    public DLLBasedDeque(){
+
+    }
+    public int size(){
+        return deque.size();
+    }
+    public boolean isEmpty(){
+        return deque.isEmpty();
+    }
+    public E first(){
+        return deque.first();
+    }
+    public E last(){
+        return deque.last();
+    }
+    public void addFirst(E e){
+        deque.addLast(e);
+    }
+    public void addLast(E e){
+        deque.addFirst(e);
+    }
+    public E removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+        return deque.removeFirst();
+    }
+    public E removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+        return deque.removeLast();
+    }
+
 }
